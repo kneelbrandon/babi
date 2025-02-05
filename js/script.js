93eb36bdb43637c95c30cf0335fee66b57daa89f
kneelbrandon <neilbrandon02@gmail.com>
2025-02-05 12:02:04 +0800

    updated home page and styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('startButton');
+    const hiddenContent1 = document.getElementById('hiddenContent1');
     const hiddenContent = document.getElementById('hiddenContent');
     const valentineAudio = document.getElementById('valentineAudio');
 
@@ -8,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             hiddenContent.style.display = 'flex';
             valentineAudio.play().catch((error) => {
                 const playButton = document.createElement('button');
-                playButton.textContent = "Play Surprise Audio";
+                playButton.textContent = "Click here!";
                 playButton.classList.add('btn', 'btn-warning', 'mt-2');
                 playButton.onclick = () => {
                     valentineAudio.play();
@@ -34,3 +35,16 @@ function createHeart() {
     document.body.appendChild(heart);
     setTimeout(() => heart.remove(), 5000);
 } 
+
+window.onload = function() {
+    var no = document.getElementById("noButton");
+    no.addEventListener("click", change);
+
+    function change() {
+        var i = Math.floor(Math.random() * (window.innerWidth - no.offsetWidth));
+        var j = Math.floor(Math.random() * (window.innerHeight - no.offsetHeight));
+        no.style.left = i + "px";
+        no.style.top = j + "px";
+    }
+};
+
